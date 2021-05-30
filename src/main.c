3427d308d9aba424e0d27d820dd9437b4f201dbc
Italo Nicola <italonicola@collabora.com>
2021-05-30 05:37:44 -0300

    wwwip

--- src/main.c
+++ src/main.c
@@ -52,11 +52,46 @@ Vector3 closestPointOnLineSegment(Vector3 A, Vector3 B, Vector3 Point) {
   return Vector3Add(A, Vector3Scale(AB, MIN(MAX(t, 0), 1)));
 }
 
-// https://wickedengine.net/2020/04/26/capsule-collision-detection/
-bool sphereCollidesTriangleEx(Vector3 center, float radius, Vector3 p0, Vector3 p1, Vector3 p2, Vector3 *penetrationVec) {
+// Adapted from https://www.scratchapixel.com/lessons/3d-basic-rendering/minimal-ray-tracer-rendering-simple-shapes/ray-sphere-intersection
+bool raySphereIntersection(Vector3 orig, Vector3 dir, Vector3 center, float radius, float *t0, float *t1) {
+#if 1
+    // geometric solution
+    Vector3 L = Vector3Subtract(center, orig);
+    float tca = Vector3DotProduct(L, dir);
+    // if (tca < 0) return false;
+    float d2 = Vector3DotProduct(L, L) - tca * tca;
+    if (d2 > radius * radius) return false;
+    float thc = sqrt(radius * radius - d2);
+    *t0 = tca - thc;
+    *t1 = tca + thc;
+#else
+    // analytic solution
+    Vec3f L = orig - center;
+    float a = dir.dotProduct(dir);
+    float b = 2 * dir.dotProduct(L);
+    float c = L.dotProduct(L) - radius2;
+    if (!solveQuadratic(a, b, c, t0, t1)) return false;
+#endif
+
+    if (*t0 > *t1) {
+        float tmp = *t0;
+        *t0 = *t1;
+        *t1 = tmp;
+    }
+
+    if (*t0 < 0) {
+        *t0 = *t1; // if t0 is negative, let's use t1 instead
+        if (*t0 < 0) return false; // both t0 and t1 are negative
+    }
+
+    return true;
+}
+
+// Adapted from https://wickedengine.net/2020/04/26/capsule-collision-detection/
+bool sphereCollidesTriangleEx(Vector3 center, float radius, Vector3 p0, Vector3 p1, Vector3 p2, Vector3 normal, Vector3 *penetrationVec) {
     Vector3 N = Vector3Normalize(Vector3CrossProduct(Vector3Subtract(p1, p0), Vector3Subtract(p2, p0))); // plane normal
     float dist = Vector3DotProduct(Vector3Subtract(center, p0), N); // signed distance between sphere and plane
-    //if(!mesh.is_double_sided() && dist > 0)
+    //if (dist > 0)
         //return false; // can pass through back side of triangle (optional)
     if (dist < -radius || dist > radius)
         return false; // no intersection
@@ -119,8 +154,15 @@ bool sphereCollidesTriangleEx(Vector3 center, float radius, Vector3 p0, Vector3 
             }
         }
 
+        float t0, t1;
+        raySphereIntersection(best_point, Vector3Scale(normal, -1), center, radius, &t0, &t1);
+        t0 += 0.01f;
+        t1 += 0.01f;
+
+        intersection_vec.y = 0;
         float len = Vector3Length(intersection_vec);
-        if (penetrationVec) *penetrationVec = Vector3Scale(Vector3Normalize(intersection_vec), radius - len);  // normalize
+        //if (penetrationVec) *penetrationVec = Vector3Scale(Vector3Normalize(intersection_vec), radius - len);  // normalize
+        if (penetrationVec) *penetrationVec = Vector3Scale(normal, t0);
 
         return true; // intersection success
     }
@@ -178,6 +220,134 @@ bool sphereCollidesTriangle(Vector3 sphere_center, float sphere_radius, Vector3 
     return !separated;
 }
 
+Vector3 vectorAbs(Vector3 v) {
+  return (Vector3) {fabs(v.x), fabs(v.y), fabs(v.z)};
+}
+
+// Adapted from https://github.com/juj/MathGeoLib/blob/master/src/Geometry/Triangle.cpp
+bool triangleAABBIntersects(Vector3 aabb_min, Vector3 aabb_max, Vector3 a, Vector3 b, Vector3 c) {
+/** The AABB-Triangle test implementation is based on the pseudo-code in
+	Christer Ericson's Real-Time Collision Detection, pp. 169-172. It is
+	practically a standard SAT test. */
+  float tMinX = MIN(a.x, MIN(b.x, c.x)); 
+  float tMinY = MIN(a.y, MIN(b.y, c.y)); 
+  float tMinZ = MIN(a.z, MIN(b.z, c.z)); 
+  float tMaxX = MAX(a.x, MAX(b.x, c.x)); 
+  float tMaxY = MAX(a.y, MAX(b.y, c.y)); 
+  float tMaxZ = MAX(a.z, MAX(b.z, c.z)); 
+
+	if (tMinX >= aabb_max.x || tMaxX <= aabb_min.x
+		|| tMinY >= aabb_max.y || tMaxY <= aabb_min.y
+		|| tMinZ >= aabb_max.z || tMaxZ <= aabb_min.z)
+		return false;
+
+	Vector3 center = Vector3Scale(Vector3Add(aabb_min, aabb_max), 0.5f);
+	Vector3 h = Vector3Subtract(aabb_max, center);
+
+	const Vector3 t[3] = { Vector3Subtract(b, a), Vector3Subtract(c, a), Vector3Subtract(c, b) };
+
+	Vector3 ac = Vector3Subtract(a, center);
+
+	Vector3 n = Vector3CrossProduct(t[0], t[1]);
+	float s = Vector3DotProduct(n, ac);
+	float r = fabs(Vector3DotProduct(h, (Vector3) {fabs(n.x), fabs(n.y), fabs(n.z)}));
+	if (fabs(s) >= r)
+		return false;
+
+	const Vector3 at[3] = { vectorAbs(t[0]), vectorAbs(t[1]), vectorAbs(t[2]) };
+
+	Vector3 bc = Vector3Subtract(b, center);
+	Vector3 cc = Vector3Subtract(c, center);
+
+	/*
+	float d1, d2, a1, a2;
+	const vec e[3] = { DIR_VEC(1, 0, 0), DIR_VEC(0, 1, 0), DIR_VEC(0, 0, 1) };
+	for(int i = 0; i < 3; ++i)
+		for(int j = 0; j < 3; ++j)
+		{
+			vec axis = Cross(e[i], t[j]);
+			ProjectToAxis(axis, d1, d2);
+			aabb.ProjectToAxis(axis, a1, a2);
+			if (d2 <= a1 || d1 >= a2) return false;
+		}
+	*/
+
+	// eX <cross> t[0]
+	float d1 = t[0].y * ac.z - t[0].z * ac.y;
+	float d2 = t[0].y * cc.z - t[0].z * cc.y;
+	float tc = (d1 + d2) * 0.5f;
+	r = fabs(h.y * at[0].z + h.z * at[0].y);
+	if (r + fabs(tc - d1) < fabs(tc))
+		return false;
+
+	// eX <cross> t[1]
+	d1 = t[1].y * ac.z - t[1].z * ac.y;
+	d2 = t[1].y * bc.z - t[1].z * bc.y;
+	tc = (d1 + d2) * 0.5f;
+	r = fabs(h.y * at[1].z + h.z * at[1].y);
+	if (r + fabs(tc - d1) < fabs(tc))
+		return false;
+
+	// eX <cross> t[2]
+	d1 = t[2].y * ac.z - t[2].z * ac.y;
+	d2 = t[2].y * bc.z - t[2].z * bc.y;
+	tc = (d1 + d2) * 0.5f;
+	r = fabs(h.y * at[2].z + h.z * at[2].y);
+	if (r + fabs(tc - d1) < fabs(tc))
+		return false;
+
+	// eY <cross> t[0]
+	d1 = t[0].z * ac.x - t[0].x * ac.z;
+	d2 = t[0].z * cc.x - t[0].x * cc.z;
+	tc = (d1 + d2) * 0.5f;
+	r = fabs(h.x * at[0].z + h.z * at[0].x);
+	if (r + fabs(tc - d1) < fabs(tc))
+		return false;
+
+	// eY <cross> t[1]
+	d1 = t[1].z * ac.x - t[1].x * ac.z;
+	d2 = t[1].z * bc.x - t[1].x * bc.z;
+	tc = (d1 + d2) * 0.5f;
+	r = fabs(h.x * at[1].z + h.z * at[1].x);
+	if (r + fabs(tc - d1) < fabs(tc))
+		return false;
+
+	// eY <cross> t[2]
+	d1 = t[2].z * ac.x - t[2].x * ac.z;
+	d2 = t[2].z * bc.x - t[2].x * bc.z;
+	tc = (d1 + d2) * 0.5f;
+	r = fabs(h.x * at[2].z + h.z * at[2].x);
+	if (r + fabs(tc - d1) < fabs(tc))
+		return false;
+
+	// eZ <cross> t[0]
+	d1 = t[0].x * ac.y - t[0].y * ac.x;
+	d2 = t[0].x * cc.y - t[0].y * cc.x;
+	tc = (d1 + d2) * 0.5f;
+	r = fabs(h.y * at[0].x + h.x * at[0].y);
+	if (r + fabs(tc - d1) < fabs(tc))
+		return false;
+
+	// eZ <cross> t[1]
+	d1 = t[1].x * ac.y - t[1].y * ac.x;
+	d2 = t[1].x * bc.y - t[1].y * bc.x;
+	tc = (d1 + d2) * 0.5f;
+	r = fabs(h.y * at[1].x + h.x * at[1].y);
+	if (r + fabs(tc - d1) < fabs(tc))
+		return false;
+
+	// eZ <cross> t[2]
+	d1 = t[2].x * ac.y - t[2].y * ac.x;
+	d2 = t[2].x * bc.y - t[2].y * bc.x;
+	tc = (d1 + d2) * 0.5f;
+	r = fabs(h.y * at[2].x + h.x * at[2].y);
+	if (r + fabs(tc - d1) < fabs(tc))
+		return false;
+
+	// No separating axis exists, the AABB and triangle intersect.
+	return true;
+}
+
 Vector3 CollideWithMap(Model mapModel, Vector3 curPos, Vector3 nextPos) {
     int reboundLen = 0;
     Vector3 rebounds[100][5];
@@ -193,16 +363,30 @@ Vector3 CollideWithMap(Model mapModel, Vector3 curPos, Vector3 nextPos) {
             Vector3 vertex2 = { mapModel.meshes[i].vertices[j+3], mapModel.meshes[i].vertices[j+4], mapModel.meshes[i].vertices[j+5]};
             Vector3 vertex3 = { mapModel.meshes[i].vertices[j+6], mapModel.meshes[i].vertices[j+7], mapModel.meshes[i].vertices[j+8]};
 
+            if (Vector3DotProduct(normal, (Vector3){0.0f, 1.0f, 0.0f}) > 0.1f) continue;
+
 #if 0
             Vector3 penetration;
-            if (sphereCollidesTriangleEx(nextPos, PLAYER_RADIUS, vertex1, vertex2, vertex3, &penetration)) {
-                rebounds[reboundLen][0] = Vector3DotProduct(); //penetration;
-#else
-            if (sphereCollidesTriangle(nextPos, PLAYER_RADIUS * 0.999f, vertex1, vertex2, vertex3)) {
-                float projection = Vector3DotProduct(Vector3Subtract(nextPos, vertex1), normal);
-                //rebounds[reboundLen][0] = Vector3Scale(normal, PLAYER_RADIUS - projection);
+            if (sphereCollidesTriangleEx(nextPos, PLAYER_RADIUS, vertex1, vertex2, vertex3, normal, &penetration)) {
+
                 Vector3 dir = Vector3Normalize(Vector3Subtract(curPos, nextPos));
-                rebounds[reboundLen][0].x = Vector3DotProduct(normal, dir);
+                if (Vector3DotProduct(penetration, dir) <= 0) continue;
+                
+                //rebounds[reboundLen][0] = penetration;
+                //rebounds[reboundLen][0].x = Vector3DotProduct(normal, dir);
+                rebounds[reboundLen][0].x = Vector3LengthSqr(Vector3Subtract(nextPos, Vector3Add(nextPos, penetration)));
+#else
+#if 0
+            if (sphereCollidesTriangle(nextPos, PLAYER_RADIUS, vertex1, vertex2, vertex3)) {
+#else
+            Vector3 aabb_min = Vector3Subtract(nextPos, (Vector3){PLAYER_RADIUS, PLAYER_RADIUS, PLAYER_RADIUS});
+            Vector3 aabb_max = Vector3Add(nextPos, (Vector3){PLAYER_RADIUS, PLAYER_RADIUS, PLAYER_RADIUS});
+            if (triangleAABBIntersects(aabb_min, aabb_max, vertex1, vertex2, vertex3)) {
+#endif
+                float projection = Vector3DotProduct(Vector3Subtract(nextPos, vertex1), normal);
+                rebounds[reboundLen][0] = Vector3Scale(normal, PLAYER_RADIUS - projection);
+                //Vector3 dir = Vector3Normalize(Vector3Subtract(curPos, nextPos));
+                //rebounds[reboundLen][0].x = Vector3DotProduct(normal, dir);
 #endif
                 rebounds[reboundLen][1] = vertex1;
                 rebounds[reboundLen][2] = vertex2;
@@ -215,7 +399,12 @@ Vector3 CollideWithMap(Model mapModel, Vector3 curPos, Vector3 nextPos) {
 
     for (int i = 1; i < reboundLen; i++) {
         for (int j = 0; j < reboundLen - i; j++) {
+#if 0
+            if (Vector3LengthSqr(rebounds[j][0]) > Vector3LengthSqr(rebounds[j + 1][0])) {
+#else
             if (rebounds[j][0].x < rebounds[j + 1][0].x) {
+            //if (rebounds[j][0].x > rebounds[j + 1][0].x) {
+#endif
                 Vector3 aux[5];
 
                 aux[0] = rebounds[j][0];
@@ -239,32 +428,57 @@ Vector3 CollideWithMap(Model mapModel, Vector3 curPos, Vector3 nextPos) {
         }
     }
 
-    //printf("reboundLen = %d\n", reboundLen);
+    printf("reboundLen = %d\n", reboundLen);
     for (int i = 0; i < reboundLen; i++) {
         //printf("trying %d [(%f, %f, %f), (%f, %f, %f), (%f, %f, %f)] ->", i, rebounds[i][1].x, rebounds[i][1].y, rebounds[i][1].z,
                                                                              //rebounds[i][2].x, rebounds[i][2].y, rebounds[i][2].z,
                                                                              //rebounds[i][3].x, rebounds[i][3].y, rebounds[i][3].z);
-        Vector3 penetration;
 #if 0
-        if (sphereCollidesTriangleEx(nextPos, PLAYER_RADIUS - 0.01f, rebounds[i][1], rebounds[i][2], rebounds[i][3], &penetration)) {
+        Vector3 penetration;
+        if (sphereCollidesTriangleEx(nextPos, PLAYER_RADIUS, rebounds[i][1], rebounds[i][2], rebounds[i][3], rebounds[i][4], &penetration)) {
             nextPos = Vector3Add(nextPos, penetration);
-            //printf("collided, size: %f", Vector3Length(penetration));
+            printf("collided, size: %f, up dot: %f, front dot: %f, inverse (wall x normal) dot: %f", Vector3Length(penetration), Vector3DotProduct(rebounds[i][4], (Vector3){0.0f,1.0f,0.0f}), Vector3DotProduct(rebounds[i][4], (Vector3){1.0f, 0.0f, 0.0f}), rebounds[i][0].x);
         }
 #else
+#if 0
         if (sphereCollidesTriangle(nextPos, PLAYER_RADIUS, rebounds[i][1], rebounds[i][2], rebounds[i][3])) {
+#else
+        Vector3 aabb_min = Vector3Subtract(nextPos, (Vector3){PLAYER_RADIUS, PLAYER_RADIUS, PLAYER_RADIUS});
+        Vector3 aabb_max = Vector3Add(nextPos, (Vector3){PLAYER_RADIUS, PLAYER_RADIUS, PLAYER_RADIUS});
+        if (triangleAABBIntersects(aabb_min, aabb_max, rebounds[i][1], rebounds[i][2], rebounds[i][3])) {
+#endif
             float projection = Vector3DotProduct(Vector3Subtract(nextPos, rebounds[i][1]), rebounds[i][4]);
 
-            Vector3 rebound = Vector3Scale(rebounds[i][4], PLAYER_RADIUS - projection);
-            nextPos = Vector3Add(nextPos, rebound);
+            Vector3 dir = Vector3Subtract(nextPos, curPos);
+            float comp1 = Vector3DotProduct(dir, rebounds[i][4]);
+            Vector3 perp = Vector3Normalize(Vector3CrossProduct(rebounds[i][4], (Vector3){0.0f, 1.0f, 0.0f}));
+            float comp2 = Vector3DotProduct(dir, perp);
 
+            //Vector3 rebound = Vector3Scale(rebounds[i][4], PLAYER_RADIUS - projection);
+            //nextPos = Vector3Add(nextPos, rebound);
+            Vector3 rebound = Vector3Add(curPos, Vector3Add(Vector3Scale(perp, comp2), Vector3Scale(rebounds[i][4], MAX(comp1, 0.0f))));
+
+            //if (Vector3Length(Vector3Subtract(nextPos, curPos)) < 0.01f) return curPos;
+
+            printf("collided, size: %f", Vector3Length(Vector3Subtract(nextPos, curPos)));
             //printf("collided, size: %f", Vector3Length(penetration));
+
+#if 0
+            if (comp2 > 0.001f || comp2 < 0.001f)
+                nextPos = rebound;
+            else
+                nextPos = curPos;
+#endif
+            nextPos = rebound;
         }
 #endif
         else {
             //printf("no collision, size: %f", Vector3Length(rebounds[i][0]));
         }
-        //printf("\n");
+        printf("\n");
     }
+
+    if (Vector3Length(Vector3Subtract(nextPos, curPos)) < 0.004f) return curPos;
 
     return nextPos;
 }
@@ -302,7 +516,7 @@ Vector3 CollideWithMapGravity(Model mapModel, Vector3 nextPos) {
         .direction = (Vector3) { 0.0f, -1.0f, 0.0f }
     };
     RayHitInfo hit = GetCollisionRayModel(ray, mapModel);
-    if (hit.hit && hit.distance < PLAYER_RADIUS) nextPos = Vector3Add(nextPos, Vector3Scale(hit.normal, PLAYER_RADIUS - hit.distance));
+    if (hit.hit) nextPos = Vector3Add(hit.position, Vector3Scale((Vector3) {0.0f, 1.0f, 0.0f}, PLAYER_RADIUS));
     return nextPos;
 }
 
@@ -335,21 +549,21 @@ void MovePlayer(Model mapModel, Player *player) {
 
     Vector3 nextPos = player->position;
     nextPos.x += deltaX * GetFrameTime();
-    player->position = CollideWithMap(mapModel, player->position, nextPos);
 
     float deltaZ = (cosf(player->cameraFPS.angle.x)*direction[MOVE_BACK] -
             cosf(player->cameraFPS.angle.x)*direction[MOVE_FRONT] +
             sinf(player->cameraFPS.angle.x)*direction[MOVE_LEFT] -
             sinf(player->cameraFPS.angle.x)*direction[MOVE_RIGHT]) / PLAYER_MOVEMENT_SENSITIVITY;
 
-    nextPos = player->position;
     nextPos.z += deltaZ * GetFrameTime();
+    Vector3 tmpDir = Vector3Scale(Vector3Normalize(Vector3Subtract(nextPos, player->position)), GetFrameTime() / PLAYER_MOVEMENT_SENSITIVITY);
+    nextPos = Vector3Add(player->position, tmpDir);
     player->position = CollideWithMap(mapModel, player->position, nextPos);
 
     float deltaY = -2.0f;
 
     nextPos = player->position;
-    nextPos.y += deltaY * GetFrameTime();
+    //nextPos.y += deltaY * GetFrameTime();
     player->position = CollideWithMapGravity(mapModel, nextPos);
 
     // Camera orientation calculation
